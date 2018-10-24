c80c50f4c40c7c63ac79ee9b70d57d24c045d475
Ethan Dennis <erdennis13@gmail.com>
2018-10-24 16:43:30 -0500

    Initial commit

---
+++ Lottie.tvOS.Sample/ViewController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Foundation;
+using UIKit;
+using Airbnb.Lottie;
+
+namespace Lottie.tvOS.Sample
+{
+    public partial class ViewController : UIViewController
+    {
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+            var lottie = LOTAnimationView.AnimationNamed("dots_spinner");
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
+
